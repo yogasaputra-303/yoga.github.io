7d926ea1cdbf9ea7a234ed45c98b58a4c84db223
yogasaputra-303 <81365518+yogasaputra-303@users.noreply.github.com>
2021-04-14 11:45:33 +0700

    Menambahkan CSS dan Java Script

---
+++ Kalkulator/script.js
@@ -0,0 +1,107 @@
+const inputNumber = (number) => {
+    if (currentNumber === '0') {
+        currentNumber = number
+    } else {
+        currentNumber += number
+    }
+}
+
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+}
+
+const numbers = document.querySelectorAll(".number");
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
+
+
+//Operator
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    if(calculationOperator === '') {
+        prevNumber = currentNumber
+    }
+    
+    calculationOperator = operator
+    currentNumber = '0'
+}
+
+//Sama Dengan
+const equalSign = document.querySelector('.equal-sign')
+
+equalSign.addEventListener('click', () => {
+    calculate()
+    updateScreen(currentNumber)
+})
+
+const calculate = () => {
+    let result = ''
+    switch(calculationOperator) {
+        case "+":
+            result = parseFloat(prevNumber) + parseFloat(currentNumber)
+            break
+        case "-":
+            result = parseFloat(prevNumber) - parseFloat(currentNumber)
+            break
+        case "*":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber)
+            break
+        case "/":
+            result = parseFloat(prevNumber) / parseFloat(currentNumber)
+            break
+        
+        default:
+            return
+    }
+    currentNumber = result
+    calculationOperator = ''
+}
+
+//Tombol AC
+const clearAll = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
+}
+const clearBtn = document.querySelector('.all-clear')
+
+clearBtn.addEventListener('click', () =>{
+    clearAll()
+    updateScreen(currentNumber)
+})
+
+
+
+//Desimal
+const decimal = document.querySelector('.decimal')
+
+inputDecimal = (dot) => {
+    if(currentNumber.includes('.')) {
+        return
+    } 
+    currentNumber+= dot
+}
+
+decimal.addEventListener('click', (event) => {
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
+
